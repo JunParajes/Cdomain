fbbb769f8ed8ad5ad9e48193e4e203202f7a3f0d
JunParajes <ethelparajes@gmail.com>
2024-05-28 12:54:02 +0800

    new portfolio

---
+++ main.js
@@ -0,0 +1,16 @@
+import './normalize.css';
+import './style.css';
+import './componentStyles/header.css';
+import './componentStyles/hero.css';
+import './componentStyles/about.css';
+import './componentStyles/featured.css';
+import './componentStyles/work.css';
+import './componentStyles/contact.css';
+import './componentStyles/footer.css';
+import './componentStyles/mobile-nav.css';
+import './utils.css';
+
+
+import mobileNav from './mobile-nav';
+
+mobileNav();
